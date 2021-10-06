7846ae4232819f1a23a8f21e2e638081423aaab7
anushab7 <69648384+anushab7@users.noreply.github.com>
2021-10-06 11:42:17 -0400

    Update Program.cs

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -21,7 +21,7 @@ namespace ConsoleApp2
                 //var conn = "mongodb+srv://systemuser_dev:ZwDjihzJzjRf44wgPMWJ@cluster0.kyanl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
                 var conn = "mongodb://systemuserdev:f7Cbd42405M8mGnjFswk@mongodb-4-4:27017";
 
-                var database = new MongoClient(conn).GetDatabase("McLarenDev");
+                var database = new MongoClient(conn).GetDatabase("Test");
                 var collection = database.GetCollection<Student>("Logs");
 
                 collection.InsertOne(new Student() { Id = 3, Name = "Prasad" });
